78b21efbacf2115675223d932da3688fcda0c70f
Sean Merrow <seanmerrow@gmail.com>
2025-06-07 23:12:35 -0400

    tested copilot

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
 def say_hello(name):
     print("Hello, " + name)
 
+def say_day_of_week(date) -> str:
+    import datetime
+    day_of_week = datetime.datetime.strptime(date, "%Y-%m-%d").strftime("%A")
+    return day_of_week
+
 say_hello("VS Code")
+
+import datetime
+print(say_day_of_week(datetime.date.today().strftime("%Y-%m-%d")))
